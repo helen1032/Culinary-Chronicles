557e77ae7fe6f81a61366b788ee09172f4f26830
Helen Cardoso <helencardoso@Helens-MacBook-Air-2.local>
2023-04-07 16:09:12 -0400

    style: added formating to the Add a Recipe page

--- HTMLFiles/JavaScript/script.js
+++ HTMLFiles/JavaScript/script.js
@@ -4,9 +4,35 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 80);
 })
 
+////Script for upload button////
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
 
+customBtn.addEventListener("click", function() {
+    realFileBtn.click();
+});
 
+realFileBtn.addEventListener("change", function(){
+    if(realFileBtn.value){
+        customTxt.innerHTML = realFileBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+    } else {
+        customTxt.innerHTML = "No File Chosen";
+    }
+});
 
+//////Popup Confirmation Script///////
+const popup = document.getElementById("popup");
+const submitBtn = document.getElementById("submitBtn");
+
+document.getElementById("submitBtn").addEventListener("click", openPopup());
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
 
 
 
